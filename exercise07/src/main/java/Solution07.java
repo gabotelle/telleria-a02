833ea5ca1d60bb49efe176560b0aecd7842132d9
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-09-17 15:28:06 -0400

    update

--- exercise07/src/main/java/Solution07.java
+++ exercise07/src/main/java/Solution07.java
@@ -6,40 +6,47 @@
 import java.util.Scanner;
 
 /*
-Prompt the user "What is the length of the room in feet?"
-    save input into <length>
-Prompt the user "What is the width of the room in feet?"
-    save input into <width>
-Print "You entered dimensions of <length> feet by <width> feet."
-Print "The area is"
-Calculate <area> = <length> * <width>
-Print "<area> square feet"
-Convert <area> from feet to meters
-Print "<area> square meters"
+Ask dimensions
+    Prompt the user "What is the length of the room in feet?"
+        save input into <length>
+    Prompt the user "What is the width of the room in feet?"
+        save input into <width>
+    Print "You entered dimensions of <length> feet by <width> feet."
+Calculate area in feet
+    Print "The area is"
+    Calculate <area> = <length> * <width>
+    Print "<areaFeet> square feet"
+Convert feet to meters
+    Convert <areaFeet> from feet to meters <areaMeters>
+    Print "<areaMeters> square meters"
 */
 public class Solution07 {
     private static final Scanner input = new Scanner(System.in);
     private static final double conversion = 0.09290304;
 
-    public static void askAreaDimesions(){
+    public static int askAreaDimensions(){
         System.out.printf("What is the length of the room in feet? ");
         int length = input.nextInt();
         System.out.printf("What is the width of the room in feet? ");
         int width = input.nextInt();
         System.out.printf("You entered dimensions of %d feet by %d feet.%n", length, width);
-        calcArea(length,width);
+        return calcAreaFeet(length,width);
     }
 
-    public static void calcArea(int length, int width){
+    public static int calcAreaFeet(int length, int width){
         System.out.printf("The area is%n");
         int areaFeet = length * width;
         System.out.printf("%d square feet%n", areaFeet);
+        return areaFeet;
+    }
+
+    public static void convertFeetToMeters(int areaFeet){
         double areaMeters = (double) areaFeet * conversion;
         System.out.printf("%.3f square meters", areaMeters);
     }
 
-
     public static void main(String[] args) {
-        askAreaDimesions();
+        int areaFeet = askAreaDimensions();
+        convertFeetToMeters(areaFeet);
     }
 }
